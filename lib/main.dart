b792992c742850c59130b8963832bf990023698f
Abdurahman AYDIN <hariysu@gmail.com>
2025-07-02 14:32:07 +0300

    Use webview instead of url launcher

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,28 @@
-import 'package:app_links/app_links.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:app_links/app_links.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatefulWidget {
+class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  State<MainApp> createState() => _MainAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(home: const HomeScreen());
+  }
 }
 
-class _MainAppState extends State<MainApp> {
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   late final AppLinks _appLinks;
 
   @override
@@ -21,7 +30,6 @@ class _MainAppState extends State<MainApp> {
     super.initState();
     _appLinks = AppLinks();
     _appLinks.allUriLinkStream.listen((Uri? uri) {
-      print("sample");
       if (uri != null) {
         final code = uri.queryParameters['code'];
         final state = uri.queryParameters['state'];
@@ -33,24 +41,127 @@ class _MainAppState extends State<MainApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () async {
-              final url = Uri.parse(
-                'https://ekapv2.kik.gov.tr/authzsvc/EkapAccount/RedirectEdevletLogin?returnUrl=uygulama://edevlet-callback',
-              );
-              if (await canLaunchUrl(url)) {
-                await launchUrl(url, mode: LaunchMode.inAppBrowserView);
-              } else {
-                throw 'Could not launch $url';
-              }
-            },
-            child: Text('Go to URL'),
-          ),
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              isScrollControlled: true,
+              builder:
+                  (context) => SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.9,
+                    child: WebViewContent(),
+                  ),
+            );
+          },
+          child: const Text('Go to URL'),
         ),
       ),
     );
   }
 }
+
+class WebViewContent extends StatefulWidget {
+  const WebViewContent({super.key});
+
+  @override
+  State<WebViewContent> createState() => _WebViewContentState();
+}
+
+class _WebViewContentState extends State<WebViewContent> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        WebViewController()
+          ..setJavaScriptMode(JavaScriptMode.unrestricted)
+          ..setNavigationDelegate(
+            NavigationDelegate(
+              onProgress: (int progress) {},
+              onPageStarted: (String url) {},
+              onPageFinished: (String url) {},
+              onHttpError: (HttpResponseError error) {},
+              onWebResourceError: (WebResourceError error) {},
+              onNavigationRequest: (NavigationRequest request) {
+                if (request.url.contains(
+                  'EkapAccount/ExternalCallBack?code=',
+                )) {
+                  print(request.url);
+                  Navigator.pop(context);
+                  return NavigationDecision.prevent;
+                }
+                return NavigationDecision.navigate;
+              },
+            ),
+          )
+          ..loadRequest(
+            Uri.parse(
+              'https://ekapv2.kik.gov.tr/authzsvc/EkapAccount/RedirectEdevletLogin?returnUrl=uygulama://edevlet-callback',
+            ),
+          );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebViewWidget(controller: controller);
+  }
+}
+
+
+/* class WebViewScreen extends StatefulWidget {
+  const WebViewScreen({super.key});
+
+  @override
+  State<WebViewScreen> createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        WebViewController()
+          ..setJavaScriptMode(JavaScriptMode.unrestricted)
+          ..setNavigationDelegate(
+            NavigationDelegate(
+              onProgress: (int progress) {
+                // Optionally handle progress
+              },
+              onPageStarted: (String url) {},
+              onPageFinished: (String url) {},
+              onHttpError: (HttpResponseError error) {},
+              onWebResourceError: (WebResourceError error) {},
+              onNavigationRequest: (NavigationRequest request) {
+                // E-devlet callback URL'ini yakala
+                if (request.url.contains(
+                  'EkapAccount/ExternalCallBack?code=',
+                )) {
+                  print(request.url);
+                  Navigator.pop(context);
+                  //_handleCallback(request.url);
+                  return NavigationDecision.prevent;
+                }
+                return NavigationDecision.navigate;
+              },
+            ),
+          )
+          ..loadRequest(
+            Uri.parse(
+              'https://ekapv2.kik.gov.tr/authzsvc/EkapAccount/RedirectEdevletLogin?returnUrl=uygulama://edevlet-callback',
+            ),
+          );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('WebView2')),
+      body: WebViewWidget(controller: controller),
+    );
+  }
+} */
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import app_links
 import url_launcher_macos
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
